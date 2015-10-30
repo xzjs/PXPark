904ab20721ce182a3ddf1ef70541de274c1f3bd9
xzjs <2310976219@qq.com>
2015-10-30 16:19:33 +0800

    测试hook文件

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('helloworld,测试钩子是否可以使用','utf-8');
+        $this->show('helloworld','utf-8');
     }
 }
