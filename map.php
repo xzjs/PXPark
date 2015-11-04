0a207c6f886d2149f01c516a1c6f33975c63809b
xzjs <2310976219@qq.com>
2015-11-04 13:00:43 +0800

    添加大数据读取

--- map.php
+++ map.php
@@ -3,45 +3,51 @@
 <head>
     <title>加载海量点</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <style type="text/css">
-        html,body{
-            margin:0;
-            width:100%;
-            height:100%;
-            background:#ffffff;
+        html, body {
+            margin: 0;
+            width: 100%;
+            height: 100%;
+            background: #ffffff;
         }
-        #map{
-            width:100%;
-            height:100%;
+
+        #map {
+            width: 100%;
+            height: 100%;
         }
+
         #panel {
             position: absolute;
-            top:30px;
-            left:10px;
+            top: 30px;
+            left: 10px;
             z-index: 999;
             color: #fff;
         }
-        #login{
-            position:absolute;
-            width:300px;
-            height:40px;
-            left:50%;
-            top:50%;
-            margin:-40px 0 0 -150px;
+
+        #login {
+            position: absolute;
+            width: 300px;
+            height: 40px;
+            left: 50%;
+            top: 50%;
+            margin: -40px 0 0 -150px;
         }
-        #login input[type=password]{
-            width:200px;
-            height:30px;
-            padding:3px;
-            line-height:30px;
-            border:1px solid #000;
+
+        #login input[type=password] {
+            width: 200px;
+            height: 30px;
+            padding: 3px;
+            line-height: 30px;
+            border: 1px solid #000;
         }
-        #login input[type=submit]{
-            width:80px;
-            height:38px;
-            display:inline-block;
-            line-height:38px;
+
+        #login input[type=submit] {
+            width: 80px;
+            height: 38px;
+            display: inline-block;
+            line-height: 38px;
         }
     </style>
     <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=您的密钥"></script>
@@ -55,9 +61,16 @@
     map.enableScrollWheelZoom();                        //启用滚轮放大缩小
     if (document.createElement('canvas').getContext) {  // 判断当前浏览器是否支持绘制海量点
         var points = [];  // 添加海量点数据
-        for (var i = 0; i < data.data.length; i++) {
-            points.push(new BMap.Point(data.data[i][0], data.data[i][1]));
-        }
+        <?php
+            $redis = new Redis();
+            $redis->connect('127.0.0.1', 6379);
+            $data=$redis->lRange('point',355002);
+            foreach ($data as $d) {
+                $a=explode('	',$d);
+                echo 'points.push(new BMap.Point('.$a[0].', '.$a[1].'));';
+            }
+            
+        ?>
         var options = {
             size: BMAP_POINT_SIZE_SMALL,
             shape: BMAP_POINT_SHAPE_STAR,
