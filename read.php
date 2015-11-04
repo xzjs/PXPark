f22aff081d8c41359682fa19859f819b9ec4cc3c
xzjs <2310976219@qq.com>
2015-11-04 12:49:51 +0800

    添加大数据读取

--- read.php
+++ read.php
@@ -9,4 +9,18 @@ $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 echo "Connection to server sucessfully";
 //查看服务是否运行
-echo "Server is running: "+ $redis->ping();
+echo "Server is running: " + $redis->ping();
+$num = $redis->lLen('point');
+if ($num) {
+    echo '已有数据';
+} else {
+    $myfile = fopen("little.txt", "r") or die("Unable to open file!");
+// 输出单行直到 end-of-file
+    while (!feof($myfile)) {
+        $s=fgets($myfile);
+        echo $s . "<br>";
+        $redis->lPush('point',$s);
+    }
+    fclose($myfile);
+    echo "读取完成";
+}
