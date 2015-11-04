2c2c47cfb84a7d069f9e772ce712bad5408fada5
xzjs <2310976219@qq.com>
2015-11-04 13:05:42 +0800

    添加大数据读取

--- map.php
+++ map.php
@@ -64,6 +64,7 @@
         <?php
             $redis = new Redis();
             $redis->connect('127.0.0.1', 6379);
+            echo "Connection to server sucessfully";
             $data=$redis->lRange('point',355002);
             foreach ($data as $d) {
                 $a=explode('	',$d);
