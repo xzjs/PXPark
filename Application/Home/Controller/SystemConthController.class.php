43dfb1afd963b2936f43ee09ab33a164b938abdf
rachel-sun <sunyx456@163.com>
2015-11-06 16:38:12 +0800

    superMangager pages (syx11.6)

---
+++ Application/Home/Controller/SystemConthController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Created by PhpStorm.
+* User: syx
+* Date: 15/11/6
+* Time: 下午15:51
+*/
+namespace Home\Controller;
+
+use Think\Controller;
+
+/**
+ * 系统管控控制器
+ * @package Home\Controller
+ */
+class SystemConthController extends Controller
+{
+	function a(){}
+}
