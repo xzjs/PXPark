4bfd109aa6ed638ced7cc58ad19903e32aec67fc
xzjs <2310976219@qq.com>
2015-11-09 09:42:00 +0800

    更改测试webhook

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('helloworld','utf-8');
+        $this->show('测试webhook','utf-8');
     }
 }
