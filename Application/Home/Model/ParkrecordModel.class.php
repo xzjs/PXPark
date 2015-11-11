c5467bc6d48fa3862ab366827613f3644b218293
xzjs <2310976219@qq.com>
2015-11-11 18:44:01 +0800

    修改停车记录查询bug

---
+++ Application/Home/Model/ParkrecordModel.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xzjs
+ * Date: 15/11/11
+ * Time: 下午4:24
+ */
+namespace Home\Model;
+use Think\Model\RelationModel;
+class ParkrecordModel extends RelationModel {
+
+}
