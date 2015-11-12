ee86fb11ee327fe21ddf7ccd5147510504351468
yxg <yanxiguoa@126.com>
2015-11-12 16:42:18 +0800

    用户注册和修改信息的前端匹配

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -28,7 +28,6 @@ class UserController extends Controller
 	 *通过网页注册用户
 	 */
 	public function web_register($username,$password,$factname,$cardNo,$phone,$message) {
-		echo "<script>window.alert(\"注册成功！\"),location.href=;</script>";//添加成功
 		$Captcha = A('Captcha');
 		$data['code'] = 0;
 		$code_status = $Captcha->verify($phone,$message);
@@ -78,7 +77,7 @@ class UserController extends Controller
 				$result=$User->add();
 			}
 		if ($result) {
-				echo "<script>window.alert(\"注册成功！\")location.href=\"{:U('Common/car_manage？id=1')}\";</script>";//添加成功
+				echo "<script>window.alert(\"注册成功！\"),location.href='".U('Common/user_info?id='.$result)."';</script>";//添加成功
 			} else {
 				echo '数据添加错误！';//添加失败
 			}
