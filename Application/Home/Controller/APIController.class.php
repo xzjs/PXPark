726e854ba108540fe20b56601087f3f33f4e044f
xzjs <2310976219@qq.com>
2015-11-12 16:16:37 +0800

    修改参数不匹配问题

--- Application/Home/Controller/APIController.class.php
+++ Application/Home/Controller/APIController.class.php
@@ -395,7 +395,7 @@ class APIController extends Controller
     public function recharge_add()
     {
         $recharge = A('Rechargerecord');
-        $reslut = $recharge->add(I('param.user_id'),I('param.money'),I('param.type'));
+        $reslut = $recharge->add(I('param.id'),I('param.money'),I('param.type'));
         if($reslut){
         	$json['code'] =0;
         	$json['msg']="正常返回";
