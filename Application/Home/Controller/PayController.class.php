92e14f4306c5211e0ae714f42c5fbc5ef375708e
yxg <yanxiguoa@126.com>
2015-11-13 10:44:24 +0800

    测试

--- Application/Home/Controller/PayController.class.php
+++ Application/Home/Controller/PayController.class.php
@@ -5,17 +5,25 @@ class PayController extends Controller {
     public function index(){
         $this->show('helloworld','utf-8');
     }
-    
-    public function pay_info($id) {
-    	$Pay=D('Pay');
-    	if($id!=0){
-    		$result=$Pay->find($id);
-    	}
-    	if($Pay->create()){
-    		$result=$Pay->add();
-    		
-    	}
-    }
+    /**
+     * 获取该用户的支付信息
+     * @param unknown $id
+     * @return unknown
+     */
+	public function pay_info($id) {
+		$result = $Pay->find ( $id );
+		return $result;
+	}
+	
+	/**
+	 * 新增支付信息
+	 */
+	public function add_pay() {
+		$Pay = D ( 'Pay' );
+		if ($Pay->create ()) {
+			$result = $Pay->add ();
+		}
+	}
     
     
 }
