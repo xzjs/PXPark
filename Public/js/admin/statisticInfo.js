45aa55af7c063a549062d17db33248339007659f
rachel-sun <sunyx456@163.com>
2015-11-18 11:32:29 +0800

    停车实时状态修改

--- Public/js/admin/statisticInfo.js
+++ Public/js/admin/statisticInfo.js
@@ -1,5 +1,5 @@
 $(function() {
-//	$('.static_right').load('parkingStateInfo.html');
+	$('.static_right').load('parkingStateInfo.html');
 	$('.sel').on('click', function() {
 		 window.location.href='/PXPark/index.php/Home/Super/statisticInfo.html';
 		
