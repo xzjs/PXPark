d262584eda4f33268ab315432123240434ec137a
yxg <yanxiguoa@126.com>
2015-11-27 15:39:31 +0800

    创建Fake控制器

---
+++ Application/Home/Controller/FakeController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+/**
+ * 模拟测试控制器
+ * createtime:2015年11月27日 下午3:38:20
+ * @author xiuge
+ */
+class FakeController extends Controller {
+	
+    public function index(){
+    
+    }
+    
+    
+    
+}
