d55cceb1df943c3254cd0f441f3fb61f4bb33484
rachel-sun <sunyx456@163.com>
2015-12-02 09:54:44 +0800

    Revert "交易管理"
    This reverts commit edb0cf7b0d4b831fb58f513e09d87166bc4cc0f1.

--- Public/js/admin/tradeManager.js
+++ Public/js/admin/tradeManager.js
@@ -7,18 +7,17 @@ var result;
 function  a(){
 	
 	var type = $('select  option:selected').val();
-	//alert(type);
+	alert(type);
 	 $.ajax({
-		 url:"../Super/tradManager",
+		 url:"../System/tradManager",
 	        type:"post",
 	        ContentType:"application/json",
 	        data:{type},
 	        async :false,
 	        success:function(data){
-	      //   alert(data);
+	        // alert(data.length);
 	           var d=eval("(" + data+ ")");
 	       result= d;
-	       //alert (result);
 	function pagerFilter(data) {
 		if ($.isArray(data)) {
 			data = {
@@ -152,14 +151,14 @@ function  a(){
 						singleSelect : true,
 						pagination : true,
 						nowrap : false,
-						pageSize : 20,
+						pageSize : 10,
 						pageList : [ 10, 20, 50, 100, 150, 200 ],
 						toolbar:'#tb',
 						columns : [ [
 								{
 									field : 'carNo',
 									title : '车牌号',
-									width : 100,
+									width : 180,
 									align : 'center'
 								},
 								{
@@ -177,7 +176,7 @@ function  a(){
 								{
 									field : 'parking',
 									title : '所停停车场',
-									width : 180,
+									width : 100,
 									align : 'center'
 								},
 								{
@@ -222,9 +221,10 @@ function  a(){
 					});
 	
 	$('#tradeGrid').datagrid({
-		data : result
+		data : getData()
 	}).datagrid('clientPaging');
 	
+	//分页工具栏上添加导出excel
 	var pager = $('#tradeGrid').datagrid('getPager');    // 得到datagrid的pager对象  
 	pager.pagination({   
 	    buttons:[{    
@@ -235,4 +235,4 @@ function  a(){
 	    }]
 	}); 
 	        }
-});}
+}
