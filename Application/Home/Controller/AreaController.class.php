3f1427b6cddd1b88266420bde239119d45820486
yxg <yanxiguoa@126.com>
2015-12-03 09:59:47 +0800

    Super控制器 zTree实现

--- Application/Home/Controller/AreaController.class.php
+++ Application/Home/Controller/AreaController.class.php
@@ -7,42 +7,40 @@ class AreaController extends Controller {
        
     }
     
-    public function get_area_list() {
-    	$Area=M('Area');
-    	$condition_sheng['level_type']=1;
-    	$sheng=$Area->where($condition_sheng)->field('id,name')->order('id')->select();
-    	$tmp=$sheng[1];
-    	$sheng[1]=$sheng[14];
-    	$sheng[14]=$tmp;
-    	for($i=0;$i<count($sheng);$i++){
-    		$json[$i]['name']=$sheng[$i]['name'];//14
-    		$json[$i]['children'][0]['name']="";
-    	}
-    	$shi=M()->query("select name from px_area where level_type=2 and parent_id=370000");
-    	for($i=0;$i<count($shi);$i++){
-    		$json[1]['children'][$i]['name']=$shi[$i]['name'];//1
-    		$json[1]['children'][$i]['children'][0]['name']="";
-    	}
-    	$qu=M()->query("select name from px_area where level_type=3 and parent_id=370200");
-    	for($i=0;$i<11;$i++){
-    		$json[1]['children'][1]['children'][$i]['name']=$qu[$i]['name'];
-    	}
-    	echo json_encode($json);
-  
-    }
-    
     public function get_area() {
     	
-    	
-    	$json[0]['name']="山东";
-    	$json[0]['children'][0]['name']="青岛";
-    	$json[0]['children'][0]['children'][0]['name']="城阳区";
-    	echo json_encode($json);
-    	
-    	
-    	
-    	
-    	
+  
     }
+	public function get_area_list($id) {
+		if ($id) {
+			$Area=M('Area');
+			$condition_sheng ['parent_id'] = $id;
+			$sheng = $Area->where ( $condition_sheng )->field ( 'id,name' )->order ( 'id' )->select ();
+			$condition['parent_id']=$sheng[0]['id'];
+			if($Area->where ( $condition )->find())
+				$isParent=true;
+			else
+				$isParent=false;
+			if($Area->where ( ))
+			for($i = 0; $i < count ( $sheng ); $i++) {
+				$json [$i] ['name'] = $sheng [$i] ['name'];
+				$json [$i] ['id'] = $sheng [$i] ['id'];
+				$json [$i] ['isParent'] = $isParent;
+				$json [$i] ['pId'] = $id;
+			}
+		} else {
+
+			$Area=M('Area');
+			$condition_sheng ['level_type'] = 1;
+			$sheng = $Area->where ( $condition_sheng )->field ( 'id,name' )->order ( 'id' )->select ();
+			for($i = 0; $i < count ( $sheng ); $i++) {
+				$json [$i] ['name'] = $sheng [$i] ['name'];
+				$json [$i] ['id'] = $sheng [$i] ['id'];
+				$json [$i] ['isParent'] = true;
+				$json [$i] ['pId'] = 0;
+			}
+		}
+		echo json_encode ( $json );
+	}
 
 }
