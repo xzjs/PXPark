78179983689f74acbf62922306b57bda051b0f0f
yxg <yanxiguoa@126.com>
2015-12-04 16:29:23 +0800

    左边栏行政区划增加一层

--- Application/Home/Controller/AreaController.class.php
+++ Application/Home/Controller/AreaController.class.php
@@ -16,43 +16,52 @@ class AreaController extends Controller {
 	 *        	用户id
 	 */
 	public function get_area($user_id) {
-		$user_type = M ()->query ( "select type from px_user where id=" . $user_id );
+	$user_type = M ()->query ( "select type from px_user where id=" . $user_id );
 		$Area = M ( 'Area' );
 		if ($user_type [0] ['type'] != 2) {
 			$area_id_list = M ()->query ( "select px_park.area_id as id,px_area.name from px_park,px_area where px_park.user_id=" . $user_id . " and px_area.id=px_park.area_id order by px_park.area_id" );
 			$json = array ();
-			$i_qu = - 1;
+			$i_jie = - 1;
+			$i_xian = - 1;
 			$i_shi = - 1;
-			$i_sheng = - 1;
-			
-			
-			
-			
+			$i_sheng=-1;
 			for($i = 0; $i < count ( $area_id_list ); $i ++) {
-				$result_qu = $area_id_list [$i];
-				$result = M ()->query ( "select a.parent_id as id,(select b.name from px_area as b where b.id=a.parent_id)  as name from px_area as a where a.id=" . $result_qu ['id'] ); // $Area->field('parent_id as id,name')->find($result_qu['id']);
+				$result_jie = $area_id_list [$i];
+				$result = M ()->query ( "select a.parent_id as id,(select b.name from px_area as b where b.id=a.parent_id)  as name from px_area as a where a.id=" . $result_jie ['id'] ); // $Area->field('parent_id as id,name')->find($result_qu['id']);
+				$result_xian = $result [0];
+				$result = M ()->query ( "select a.parent_id as id,(select b.name from px_area as b where b.id=a.parent_id)  as name from px_area as a where a.id=" . $result_xian ['id'] );
 				$result_shi = $result [0];
 				$result = M ()->query ( "select a.parent_id as id,(select b.name from px_area as b where b.id=a.parent_id)  as name from px_area as a where a.id=" . $result_shi ['id'] );
 				$result_sheng = $result [0];
 				if ($result_sheng ['name'] != $tmp_sheng) {
-					$i_sheng ++;
-					$i_shi = 0;
-					$i_qu = 0;
+					$i_sheng++;
+					$i_shi =0;
+					$i_xian = 0;
+					$i_jie = 0;
 					$json [$i_sheng] ['name'] = $result_sheng ['name'];
-					$json [$i_sheng] ['children'] [$i_shi] ['name'] = $result_shi ['name'];
-					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_qu] ['name'] = $result_qu ['name'];
-				} elseif ($result_shi ['name'] != $tmp_shi) {
+					$json [$i_sheng] ['children'][$i_shi] ['name'] = $result_shi ['name'];
+					$json [$i_sheng] ['children'][$i_shi] ['children'] [$i_xian] ['name'] = $result_xian ['name'];
+					$json [$i_sheng] ['children'][$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+				}elseif ($result_shi ['name'] != $tmp_shi) {
 					$i_shi ++;
-					$i_qu = 0;
+					$i_xian = 0;
+					$i_jie = 0;
 					$json [$i_sheng] ['children'] [$i_shi] ['name'] = $result_shi ['name'];
-					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_qu] ['name'] = $result_qu ['name'];
-				} elseif ($result_qu ['name'] != $tmp_qu) {
-					$i_qu ++;
-					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_qu] ['name'] = $result_qu ['name'];
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['name'] = $result_xian ['name'];
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+				} elseif ($result_xian ['name'] != $tmp_xian) {
+					$i_xian ++;
+					$i_jie = 0;
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['name'] = $result_xian ['name'];
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+				} elseif ($result_jie ['name'] != $tmp_jie) {
+					$i_jie ++;
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
 				}
-				$tmp_sheng = $result_sheng ['name'];
+				$tmp_sheng= $result_sheng['name'];
 				$tmp_shi = $result_shi ['name'];
-				$tmp_qu = $result_qu ['name'];
+				$tmp_xian = $result_xian ['name'];
+				$tmp_jie = $result_jie ['name'];
 			}
 			echo json_encode ( $json );
 		} else {
