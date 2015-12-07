f571e3149c81933d327f19c8b56db66d517c021b
xzjs <2310976219@qq.com>
2015-12-07 10:34:28 +0800

    去除自动脚本中的错误

--- Public/js/admin/main.js
+++ Public/js/admin/main.js
@@ -3,40 +3,28 @@
  * 
  * @param page
  */
-$(function() {
-	//checsessoin();
-})
-function checsessoin()
-   {//alert('请先登录');
-      var strSession = "<%=SESSION['id'] %>"; 
-       if( strSession == ""||strSession==null)
-       {
-          alert('请先登录');
-          window.location.href="/PXPark/index.php/Home/Index/index.html";
-        }
-       else{
-    	   alert("ff"+strSession);
-       }
-    }
-function pageJump(page, me, flag) {
-	$('#content').load(page);
-	
+function pageJump() {
+	//$('#content').load(page);
+	var me= $.cookie('me');
+    var flag= $.cookie('flag');
 	if(flag == '0') {
 		removeNavBG();
-		$(me).addClass('nav_selected');
+		$('#'+me).addClass('nav_selected');
 	} else if(flag == 1){
 		removeNavBG();
-		$(me).parent().parent().addClass('nav_selected');
-		$(me).addClass('nav_selected');
-		$(me).parent().parent().blur();
+		$('#'+me).parent().parent().addClass('nav_selected');
+		$('#'+me).addClass('nav_selected');
+		$('#'+me).parent().parent().blur();
 	} else if(flag == 3){
 		removeNavBG();
-		$(me).parent().addClass('nav_selected');
-		$(me).parent().children().eq(1).children().eq(0).addClass('nav_selected');
-		$(me).blur();
+		$('#'+me).parent().addClass('nav_selected');
+		$('#'+me).parent().children().eq(1).children().eq(0).addClass('nav_selected');
+		$('#'+me).blur();
 	}
 }
-function pageJump1(page){
+function pageJump1(page,me,flag){
+    $.cookie('me',me);
+    $.cookie('flag',flag);
 	window.location.href=page;
 }
 function removeNavBG() {
