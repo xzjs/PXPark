362b4390978076c7d14134f0bb12e4751ebc9801
yxg <yanxiguoa@126.com>
2015-12-09 09:45:07 +0800

    时间插件选择限制

---
+++ Public/js/time-util.js
@@ -0,0 +1,106 @@
+/**
+ * ajax图表统计获取数据
+ * 
+ * @param pageName
+ *            页面名字
+ * @param url
+ *            访问链接
+ * @param start
+ *            开始时间
+ * @param end
+ *            结束时间
+ * @param car_category
+ *            车的类型
+ */
+function getInfo(pageName, url, start, end, car_category) {
+	if ((!start) && (!end)) {
+		start = "2015-12-1";
+		end = getNowFormatDate();
+	}
+
+	$.post(url, {
+		start_time : start,
+		end_time : end,
+		type : car_category,
+	}, function(datas, status) {
+		if (status == 4 || status == "success") {
+			if (pageName == 'car_manage') {
+				var data = JSON.parse(datas);
+				$('#personGrid').datagrid('loadData', data).datagrid(
+						'clientPaging');
+				$("span#in_num").text(data.in_num);
+				$("span#out_num").text(data.finish_num);
+				$("span#money").text(data.money);
+			} else if (pageName == 'count_income') {
+				var data = JSON.parse(datas);
+				$('#personGrid').datagrid('loadData', data).datagrid(
+						'clientPaging');
+				$("span#in_num").text(data.in_num);
+				$("span#out_num").text(data.finish_num);
+				$("span#money").text(data.money);
+			} else if (pageName == 'park_analyse') {
+				var lineInfo = eval(datas);
+				drawChart(lineInfo);
+			}
+		}
+	});
+
+}
+
+/**
+ * 根据一个时间插件的值，生成对另一个时间插件的选择限制
+ * 
+ * @param sore
+ */
+function timeLimit(sore) {
+	if (sore == 'start') {
+		$('#income_end_time')
+				.attr(
+						"onclick",
+						"laydate({istime: true, format: 'YYYY-MM-DD hh:mm:ss',min:getStartTime(),max:getNowFormatDate()})")
+	} else if (sore == 'end') {
+		$('#income_start_time')
+				.attr("onclick",
+						"laydate({istime: true, format: 'YYYY-MM-DD hh:mm:ss',max:getEndTime()})")
+	}
+
+}
+
+/**
+ * 获取开始插件的时间
+ * 
+ * @returns 开始时间
+ */
+function getStartTime() {
+	return $('input#income_start_time').val();
+}
+
+/**
+ * 获取结束插件的时间
+ * 
+ * @returns 结束时间
+ */
+function getEndTime() {
+	return $('input#income_end_time').val();
+}
+
+/**
+ * 获取当前时间 格式为：yyyy-MM-dd HH:MM:SS
+ */
+function getNowFormatDate() {
+	var date = new Date();
+	var seperator1 = "-";
+	var seperator2 = ":";
+	var month = date.getMonth() + 1;
+	var strDate = date.getDate();
+	if (month >= 1 && month <= 9) {
+		month = "0" + month;
+	}
+	if (strDate >= 0 && strDate <= 9) {
+		strDate = "0" + strDate;
+	}
+	var currentdate = date.getFullYear() + seperator1 + month + seperator1
+			+ strDate + " " + date.getHours() + seperator2 + date.getMinutes()
+			+ seperator2 + date.getSeconds();
+	return currentdate;
+}
