9ff4edc60bbab6cbab2c0a8391b28466938e3319
yxg <yanxiguoa@126.com>
2015-12-10 17:18:09 +0800

    左边栏地域列表增加一级停车场

--- Application/Home/Controller/AreaController.class.php
+++ Application/Home/Controller/AreaController.class.php
@@ -19,14 +19,17 @@ class AreaController extends Controller {
 	$user_type = M ()->query ( "select type from px_user where id=" . $user_id );
 		$Area = M ( 'Area' );
 		if ($user_type [0] ['type'] != 2) {
-			$area_id_list = M ()->query ( "select px_park.area_id as id,px_area.name from px_park,px_area where px_park.user_id=" . $user_id . " and px_area.id=px_park.area_id order by px_park.area_id" );
+			$park_list = M ()->query ( "select id,name from px_park where user_id=" . $user_id . " order by area_id" );
 			$json = array ();
+			$i_park=-1;
 			$i_jie = - 1;
 			$i_xian = - 1;
 			$i_shi = - 1;
 			$i_sheng=-1;
-			for($i = 0; $i < count ( $area_id_list ); $i ++) {
-				$result_jie = $area_id_list [$i];
+			for($i = 0; $i < count ( $park_list ); $i ++) {
+				$result_park=$park_list[$i];
+				$result = M ()->query ( "select a.area_id as id,(select b.name from px_area as b where b.id=a.area_id)  as name from px_park as a where a.id=" . $result_park ['id'] ); // $Area->field('parent_id as id,name')->find($result_qu['id']);
+				$result_jie = $result [0];
 				$result = M ()->query ( "select a.parent_id as id,(select b.name from px_area as b where b.id=a.parent_id)  as name from px_area as a where a.id=" . $result_jie ['id'] ); // $Area->field('parent_id as id,name')->find($result_qu['id']);
 				$result_xian = $result [0];
 				$result = M ()->query ( "select a.parent_id as id,(select b.name from px_area as b where b.id=a.parent_id)  as name from px_area as a where a.id=" . $result_xian ['id'] );
@@ -38,32 +41,45 @@ class AreaController extends Controller {
 					$i_shi =0;
 					$i_xian = 0;
 					$i_jie = 0;
+					$i_park=0;
 					$json [$i_sheng] ['name'] = $result_sheng ['name'];
 					$json [$i_sheng] ['children'][$i_shi] ['name'] = $result_shi ['name'];
 					$json [$i_sheng] ['children'][$i_shi] ['children'] [$i_xian] ['name'] = $result_xian ['name'];
 					$json [$i_sheng] ['children'][$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+					$json [$i_sheng] ['children'][$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['children'][$i_park]['name'] = $result_park ['name'];
 				}elseif ($result_shi ['name'] != $tmp_shi) {
 					$i_shi ++;
 					$i_xian = 0;
 					$i_jie = 0;
+					$i_park=0;
 					$json [$i_sheng] ['children'] [$i_shi] ['name'] = $result_shi ['name'];
 					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['name'] = $result_xian ['name'];
 					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['children'][$i_park]['name'] = $result_park ['name'];
 				} elseif ($result_xian ['name'] != $tmp_xian) {
 					$i_xian ++;
 					$i_jie = 0;
+					$i_park=0;
 					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['name'] = $result_xian ['name'];
 					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['children'][$i_park]['name'] = $result_park ['name'];
 				} elseif ($result_jie ['name'] != $tmp_jie) {
 					$i_jie ++;
+					$i_park=0;
 					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['name'] = $result_jie ['name'];
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['children'][$i_park]['name'] = $result_park ['name'];
+				} elseif($result_park['name']!=$tmp_park){
+					$i_park++;
+					$json [$i_sheng] ['children'] [$i_shi] ['children'] [$i_xian] ['children'] [$i_jie] ['children'][$i_park]['name'] = $result_park ['name'];
 				}
 				$tmp_sheng= $result_sheng['name'];
 				$tmp_shi = $result_shi ['name'];
 				$tmp_xian = $result_xian ['name'];
 				$tmp_jie = $result_jie ['name'];
+				$tmp_park=$result_park['name'];
 			}
 			echo json_encode ( $json );
+			//var_dump(json_encode($json));
 		} else {
 			echo 2;
 		}
@@ -76,28 +92,47 @@ class AreaController extends Controller {
 	public function get_area_list($id) {
 		if ($id) {
 			$Area = M ( 'Area' );
-			$condition_sheng ['parent_id'] = $id;
-			$sheng = $Area->where ( $condition_sheng )->field ( 'id,name' )->order ( 'id' )->select ();
-			$condition ['parent_id'] = $sheng [0] ['id'];
-			if ($Area->where ( $condition )->find ())
-				$isParent = true;
-			else
-				$isParent = false;
-			if ($Area->where ())
-				for($i = 0; $i < count ( $sheng ); $i ++) {
-					$json [$i] ['name'] = $sheng [$i] ['name'];
-					$json [$i] ['id'] = $sheng [$i] ['id'];
-					$json [$i] ['isParent'] = $isParent;
+			$Park=M('Park');
+			$cdt_children ['parent_id'] = $id;
+			$children = $Area->where ( $cdt_children )->field ( 'id,name' )->order ( 'id' )->select ();
+			if ($children) {
+				
+				for($i = 0; $i < count ( $children ); $i ++) {
+					$condition_children['parent_id']=$children [$i]['id'];
+					$condition_park['area_id']=$children [$i]['id'];
+					$has_children=$Area->where ($condition_children)->find ();
+					$has_park=$Park->where ($condition_park)->find ();
+					
+					
+					if ($has_children||$has_park){
+						$json [$i] ['isParent'] = true;
+					}else{
+						$json [$i] ['isParent'] = false;
+					}
+					$json [$i] ['name'] = $children [$i] ['name'];
+					$json [$i] ['id'] = $children [$i] ['id'];
 					$json [$i] ['pId'] = $id;
 				}
+			}else{
+				$condition_park['area_id']=$id;
+				$park=$Park->where ($condition_park)->field ( 'id,name' )->order ( 'id' )->select ();
+				
+				for($i = 0; $i < count ( $park ); $i ++) {
+					
+					$json [$i] ['isParent'] = false;
+					$json [$i] ['name'] = $park [$i] ['name'];
+					$json [$i] ['id'] = $id."-".$park [$i] ['id'];
+					$json [$i] ['pId'] = $id;
+				}
+			}
 		} else {
 			
 			$Area = M ( 'Area' );
-			$condition_sheng ['level_type'] = 1;
-			$sheng = $Area->where ( $condition_sheng )->field ( 'id,name' )->order ( 'id' )->select ();
-			for($i = 0; $i < count ( $sheng ); $i ++) {
-				$json [$i] ['name'] = $sheng [$i] ['name'];
-				$json [$i] ['id'] = $sheng [$i] ['id'];
+			$cdt_children ['level_type'] = 1;
+			$children = $Area->where ( $cdt_children )->field ( 'id,name' )->order ( 'id' )->select ();
+			for($i = 0; $i < count ( $children ); $i ++) {
+				$json [$i] ['name'] = $children [$i] ['name'];
+				$json [$i] ['id'] = $children [$i] ['id'];
 				$json [$i] ['isParent'] = true;
 				$json [$i] ['pId'] = 0;
 			}
